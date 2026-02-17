5f023cc75c76fd94e089ba5730db39944e963546
SureshThapa <sambridthapa6@gmail.com>
2026-02-17 19:39:31 +0545

    baseurl name changed to /api added

--- src/api/bestDealsProductApi.js
+++ src/api/bestDealsProductApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: "https://jagdamba-website-backend.vercel.app",
+  baseURL: "https://jagdamba-website-backend.vercel.app/api",
 //  baseURL: "http://localhost:5000/api",
 });
 
--- src/api/cartApi.js
+++ src/api/cartApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://jagdamba-website-backend.vercel.app",
+  baseURL: "https://jagdamba-website-backend.vercel.app/api",
 });
 
 API.interceptors.request.use((req) => {
