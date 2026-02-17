2393001e85b489c1614d3514e6d2d13f957e9d1a
SureshThapa <sambridthapa6@gmail.com>
2026-02-17 19:13:36 +0545

    final casing fix

--- src/components/BestDealsProduct.jsx
+++ src/components/BestDealsProduct.jsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useNavigate } from "react-router-dom";
-import { getBestDealProduct} from "../api/temp";
+import { getBestDealProduct} from "../api/bestDealsProductApi"
+";
 
 
 function BestDealsProduct() {
